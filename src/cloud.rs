343d52ef969976bd35d6991d87e777a298bfe132
Jorge Aparicio <jorge@japaric.io>
2017-05-16 16:40:30 -0500

    safe API for spark_cloud

--- src/cloud.rs
+++ src/cloud.rs
@@ -1,19 +1,46 @@
 use core::ptr;
 
-use ll;
+use {String, ll};
+
+pub fn function(
+    name: &str,
+    f: extern "C" fn(&String) -> i32,
+) -> Result<(), ()> {
+    // the max length of the variable name is 12 characters
+    if name.len() > 12 {
+        return Err(());
+    }
+
+    // null terminated string
+    let mut buffer = [0; 13];
+    buffer[..name.len()].copy_from_slice(name.as_bytes());
+
+    if unsafe {
+        ll::spark_function(buffer.as_ptr() as *const _, f, ptr::null_mut())
+    }
+    {
+        Ok(())
+    } else {
+        Err(())
+    }
+}
 
 pub fn variable(name: &str, variable: &'static i32) -> Result<(), ()> {
-    // null terminated string
     // the max length of the variable name is 12 characters
+    if name.len() > 12 {
+        return Err(());
+    }
+
+    // null terminated string
     let mut buffer = [0; 13];
-    buffer[..12][..name.len()].copy_from_slice(name.as_bytes());
+    buffer[..name.len()].copy_from_slice(name.as_bytes());
 
     if unsafe {
         ll::spark_variable(
             buffer.as_ptr() as *const _ as *const _,
             variable as *const _ as *const _,
             ll::Spark_Data_TypeDef::CLOUD_VAR_INT,
-            ptr::null_mut()
+            ptr::null_mut(),
         )
     }
     {
